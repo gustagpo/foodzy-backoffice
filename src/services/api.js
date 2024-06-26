13bd0d76e9debb01388bfcf50f757ba710a06cea
GP Soluções <gustavo@teamgp.com.br>
2024-06-26 00:21:06 -0300

    ajuste api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333/api/v1/'
-    // baseURL: 'https://ws.fxbank.com.br/api/v1/'
+    baseURL: 'https://ws.fxbank.com.br/api/v1/'
 });
 
 export default api;
